dc052d25c2e1014208f63dfe84321652281daa10
Toshiyuki HINA <thina@yourinventit.com>
2014-07-15 17:25:04 +0900

    Adds accordion directive.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,14 @@
 
 angular.module('angularDirectiveExpanderApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.title = 'Click to show';
-    $scope.text = 'Hello everybody! Now shown.';
+    $scope.expanders = [{
+      title: 'クリックして表示',
+      text: '皆さんこんにちは。非表示でしたが、今は表示されています。'
+    }, {
+      title: 'クリックしてください',
+      text: '今までに表示されていたものよりはましなコンテンツ'
+    }, {
+      title: 'こちらをクリック!',
+      text: 'こちらを先に表示させるべきでした。'
+    }];
   }]);
--- app/scripts/directives/expander.js
+++ app/scripts/directives/expander.js
@@ -1,25 +1,26 @@
 'use strict';
 
 angular.module('angularDirectiveExpanderApp')
-  .directive('expander', function () {
+  .directive('expander', ['$log', function ($log) {
     return {
       restrict: 'EA',
       replace: true,
       transclude: true,
+      require: '^?accordion',
       scope: { title: '=expanderTitle' },
-      template: '<div>' + 
-        '<div class="title">{{title}}</div>' +
-        '<div class="body closed" ng-transclude></div>' + 
+      template: '<div>' +
+        '<div class="title" ng-click="toggle()">{{title}}</div>' +
+        '<div class="body" ng-show="showMe" ng-transclude></div>' +
         '</div>',
-      link: function postLink(scope, element, attrs) {
-        var titleElement = angular.element(element.children().eq(0));
-        var bodyElement = angular.element(element.children().eq(1));
-
-        titleElement.bind('click', toggle);
-
-        function toggle() {
-          bodyElement.toggleClass('closed');
-        }
+      link: function(scope, element, attrs, accordionController) {
+        $log.debug('* postLink');
+        scope.showMe = false;
+        accordionController.addExpander(scope);
+        scope.toggle = function() {
+          $log.debug('* toggle@postLink');
+          scope.showMe = !scope.showMe;
+          accordionController.gotOpened(scope);
+        };
       }
     };
-  });
+  }]);
